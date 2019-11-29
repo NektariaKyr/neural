6aca0c265e10ed7abff173b2b2ea128f454e6a02
NektariaKyr <33091903+NektariaKyr@users.noreply.github.com>
2019-11-29 11:49:21 +0200

    Update neural1.py

--- neural1.py
+++ neural1.py
@@ -3,12 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-weight = np.array([[0.1 , 0.2 , 0.3],
-                 [0.4 , 0.5, 0.6],
-                 [0.7, 0.8, 0.9],
-                 [0.07, 0.09, 0.04]])
-
-
+weight_1=np.random.rand(4,3)
+weight_2=np.random.rand(5,4)
+weight_3=np.random.rand(4,3)
+weight_4=np.random.rand(3,5)
+weight=[weight_1,weight_2,weight_3,weight_4]
 input=[2,9,10]
 
 def sigmoid(x):
@@ -17,34 +16,26 @@ def sigmoid(x):
     return a
 
 def neuron(inputs,weights):
- weighted_sum=0
- for i in range (0,len(input)):
-    weighted_sum=weights[i]*inputs[i]+weighted_sum
- tr_function=sigmoid(weighted_sum)
- return tr_function
+    weighted_sum=0
+    for i in range (0,len(input)):
+        weighted_sum=weights[i]*inputs[i]+weighted_sum
+        tr_function=sigmoid(weighted_sum)
+    return tr_function
 
 
 def layer(input,weights):
-    o=[]
-    for i in range (0,len(input)):
-        y=neuron(input,weight[i])
-        o.append(y)
-    return(o)
+    out=[]
+    for i in range (0,len(weights)):
+        y=neuron(input,weights[i])
+        out.append(y)
+    return(out)
 
-layer_1_numbers=layer(input,weight)
-print(layer_1_numbers)
-
-def network (input,weights):
-    d=[]
+def network (input,weights_all):
     #getting into each  layer one at a time
-    for i in range (0,10):
-        layer_i=layer(input,weights)
-        input=layer_i
-        #generating the outputs of each layer
-        for i in range (0,len(layer_i)):
-            y=layer_i[i]
-            j=layer(input,weights)
-            d.append(y)
-    return(d)
+    for i in weights_all:
+        layer_i=layer(input,i)
+        print(layer_i)
  
-print(network(input,weight))
+y=network(input,weight)
+
+
